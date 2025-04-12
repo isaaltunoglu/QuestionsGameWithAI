011ee60d1b95c36fb3d02e4eeefc5b44da4811f1
İsa Altunoğlu <146170165+isaaltunoglu@users.noreply.github.com>
2025-04-12 18:45:35 +0300

    finished alghoritm waiting for GUI

--- main.py
+++ main.py
@@ -1,32 +1,73 @@
-# ilk olarak oyunun ana mekanizmasını yazacağız ve şöyle olmalı gemini ai ' in apisini kullanarak 
-#sorualrı metin olarak çekeceğiz ve doğru cevabıda metinin alt kısımında verecek ve biz o doğru cevabı splitmetodu ile ayırıp
-#kullanıcıdan bir cevap alcağaız ve doğru cevap ile karşılaştırırdığımızda eğer doğruysa kullanıcıya puan vereceğiz be bir sonraki
-# soruya geçeceğiz eğer yanlış bir cevap verdiyse Kullanıcı ana ekranda doğru cevap buydu deyip tekrar oyanamak ister misin ekranı gelecek
+
+import re
+from google import genai
+# This is a simple quiz game that generates questions using the Gemini API.
+
+class Game():
+    def __init__(self):
+        self.score = 0
+        self.index = 0
+        self.questions = []
+        self.options = []
+        self.right_answers = []
+        self.text = ""
 
 
-
-questions = ["What is the capital of France?"] #Example
-answers = ["A)Ankara B)Berlin C)Moscow D)Paris"]
-right_answer = ["D"] #Example
-useranswer = ""
-score = 0
-i = 0
+    def create_text(self):
+        client = genai.Client(api_key="YOUR-API-KEY HERE") #You can copy your Gemini API key from the Google Cloud Console.
 
 
-while True:
-    print(questions[i])
-    print(answers[i])
-    useranswer = str(input("Cevabınız Nedir: ")).capitalize()
-    if useranswer == right_answer[i]:
-        score += 10
-        i += 1
-    else:
-        print(f"You lost. Your Score: {score}")
+        #Create a response with your own prompt but dont change prompt but you can translate other language.
+        response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=''' 
+        Lütfen aşağıdaki kurallara uygun şekilde 10 adet bilgi ve genel kültür sorusu üret:
+        Her soru **** (iki yıldız) ile başlamalıdır.
+        Her sorunun dört adet şıkkı olmalıdır. Şıklar a, b, c, d ile gösterilmeli ve her şıkkın başında & sembolü olmalıdır.
+        Şıklardan sonra "Cevap Anahtarı" başlığı altında her sorunun doğru cevabını belirt.
+        Cevap anahtarında her satır ! sembolüyle başlamalıdır ve 1. b, 2. c formatında olmalıdır.
+        ''',)
+        
+        self.text = response.text
 
+    def text_separater(self):
+        self.text = self.text.splitlines()
+        for line in self.text:
+            if "*" in line:
+                self.questions.append(line)
+            if "!" in line:
+                letter = re.search(r'([a-d])',line)
+                self.right_answers.append(letter.group(1))
+            if "&" in line:
+                self.options.append(line)
 
+    def init_game(self):
+        self.score = 0
+        self.index = 0
+        self.questions = []
+        self.options = []
+        self.right_answers = []
+        self.create_text()
+        self.text_separater()
+        while self.index < len(self.questions):
+            print(self.questions[self.index])
+            j = 4*self.index
+            while j < (4 + self.index*4):
+                print(self.options[j])
+                j+=1
+            useranswer = str(input("Cevabınız Nedir: "))
+            if useranswer.lower() == str(self.right_answers[self.index]).lower():
+                self.score += 10
+                self.index += 1
+            else:
+                print(f"You lost. Your Score: {self.score}")
+                choice = input("do you want to continue Y/n").lower()
+                if choice == "y":
+                    self.init_game()
+                else:
+                    break
 
-
-
-
-
+if __name__ == "__main__":
+    game = Game()
+    game.init_game()
 
